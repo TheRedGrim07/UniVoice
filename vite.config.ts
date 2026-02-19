a9b28f39fab2af9559aa4efcf04e78d3ed185ade
Amitangshu Kar <amitangshukar1994@gmail.com>
2026-02-19 21:26:45 +0530

    Squashed 'univoice-frontend/' content from commit b238bec
    git-subtree-dir: univoice-frontend
    git-subtree-split: b238bec13f1973e921f5a9269acc1e340280b127

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+ resolve: {
+  alias: {
+    "@": path.resolve(__dirname, "./src"),
+  },
+},
+}));
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
+
